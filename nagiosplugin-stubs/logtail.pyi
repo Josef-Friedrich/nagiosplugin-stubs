d0c0835476b5545a57612a9b2a0a620a8e77a977
Josef Friedrich <josef@friedrich.rocks>
2022-07-28 17:34:59 +0200

    Add some type hints

--- nagiosplugin-stubs/logtail.pyi
+++ nagiosplugin-stubs/logtail.pyi
@@ -1,8 +1,14 @@
-from typing import Generator
+from types import TracebackType
+from typing import Generator, Optional, Type
 
 from nagiosplugin.cookie import Cookie
 
 class LogTail:
     def __init__(self, path: str, cookie: Cookie) -> None: ...
     def __enter__(self) -> Generator[bytes, None, None]: ...
-    def __exit__(self, exc_type, exc_val, exc_tb) -> None: ...
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType],
+    ) -> Optional[bool]: ...
