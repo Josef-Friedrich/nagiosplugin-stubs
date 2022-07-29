3369633c2c902d8c1c1a28d9c13956529bfc9dd0
Josef Friedrich <josef@friedrich.rocks>
2022-07-29 17:50:23 +0200

    Add some types

--- nagiosplugin-stubs/metric.pyi
+++ nagiosplugin-stubs/metric.pyi
@@ -5,12 +5,23 @@ from nagiosplugin.context import Context
 from nagiosplugin.performance import Performance
 from nagiosplugin.resource import Resource
 from nagiosplugin.result import Result
+from typing_extensions import TypedDict, Unpack
+
+class MetricKwargs(TypedDict, total=False):
+    name: str
+    value: Any
+    uom: str
+    min: int | float
+    max: int | float
+    context: str
+    contextobj: Context
+    resource: Resource
 
 @dataclass
 class Metric:
 
-    name: str | None
-    value: Any | None
+    name: str
+    value: Any
     uom: str | None = None
     min: int | float | None = None
     max: int | float | None = None
@@ -18,7 +29,7 @@ class Metric:
     contextobj: Context | None = None
     resource: Resource | None = None
 
-    def replace(self, **attr) -> Metric: ...
+    def replace(self, **attr: Unpack[MetricKwargs]) -> Metric: ...
     @property
     def description(self) -> str: ...
     @property
