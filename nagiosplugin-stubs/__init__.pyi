5821699d0671e1e0c82d518101b7ce80b32ab58a
Josef Friedrich <josef@friedrich.rocks>
2022-11-26 17:32:43 +0100

    Fix reimports
    to be able to import for example nagiosplugin.Check not
    nagiosplugin.check.Check
    
    From PEP 484
    
    Modules and variables imported into the stub are not considered
    exported from the stub unless the import uses the
    import ... as ... form or the equivalent
    from ... import ... as ... form.

--- nagiosplugin-stubs/__init__.pyi
+++ nagiosplugin-stubs/__init__.pyi
@@ -1,17 +1,22 @@
-from nagiosplugin.check import Check
-from nagiosplugin.context import Context, ScalarContext
-from nagiosplugin.cookie import Cookie
-from nagiosplugin.error import CheckError, Timeout
-from nagiosplugin.logtail import LogTail
-from nagiosplugin.metric import Metric
-from nagiosplugin.multiarg import MultiArg
-from nagiosplugin.performance import Performance
-from nagiosplugin.range import Range
-from nagiosplugin.resource import Resource
-from nagiosplugin.result import Result, Results
-from nagiosplugin.runtime import Runtime, guarded
-from nagiosplugin.state import Critical, Ok, Unknown, Warn
-from nagiosplugin.summary import Summary
+from nagiosplugin.check import Check as Check
+from nagiosplugin.context import Context as Context, ScalarContext as ScalarContext
+from nagiosplugin.cookie import Cookie as Cookie
+from nagiosplugin.error import CheckError as CheckError, Timeout as Timeout
+from nagiosplugin.logtail import LogTail as LogTail
+from nagiosplugin.metric import Metric as Metric
+from nagiosplugin.multiarg import MultiArg as MultiArg
+from nagiosplugin.performance import Performance as Performance
+from nagiosplugin.range import Range as Range
+from nagiosplugin.resource import Resource as Resource
+from nagiosplugin.result import Result as Result, Results as Results
+from nagiosplugin.runtime import Runtime as Runtime, guarded as guarded
+from nagiosplugin.state import (
+    Critical as Critical,
+    Ok as Ok,
+    Unknown as Unknown,
+    Warn as Warn,
+)
+from nagiosplugin.summary import Summary as Summary
 from nagiosplugin.version import __VERSION__
 
 __version__: str = ...
